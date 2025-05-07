11be7b4834b4b192946a282b07a49f0822bdec95
lZh1r <electroxhackerpig09@gmail.com>
2025-05-07 19:48:23 +0300

    title autofocus on note creation

--- src/elements/NoteCard.tsx
+++ src/elements/NoteCard.tsx
@@ -1,16 +1,15 @@
 import {Link} from "react-router";
 import {faEllipsisVertical} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {useState} from "react";
+import {RefObject, useState} from "react";
 import {useStore} from "../utils/AppState.ts";
 
-function NoteCard({title, text, path, id}: {title: string, text: string, path: string, id: string}) {
+function NoteCard({title, text, path, id, ref}: {title: string, text: string, path: string, id: string, ref: RefObject<any>}) {
 
     const [noteTitle, setNoteTitle] = useState(title);
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const changeNoteTitle = useStore.getState().changeNoteTitle;
     const deleteNote = useStore.getState().deleteNote;
-
     function updateTitle() {
         changeNoteTitle(id, noteTitle);
     }
@@ -19,9 +18,9 @@ function NoteCard({title, text, path, id}: {title: string, text: string, path: s
             <div className="font-main bg-element border-inactive border-2 border-solid rounded-2xl text-inactive hover:text-active
                 hover:border-active p-3 m-3 transition-all">
                 <div className="flex flex-row justify-between">
-                    <input onBlur={updateTitle} type="text" onChange={(e) => setNoteTitle(e.currentTarget.value)}
+                    <input onBlur={updateTitle} type="text" ref={ref} onChange={(e) => setNoteTitle(e.currentTarget.value)}
                            value={noteTitle}
-                           className="text-2xl cursor-text focus:outline-0 focus:text-active border-active focus:border-b-2 w-[10rem]"/>
+                           className="text-2xl cursor-text focus:outline-0 focus:text-active border-active focus:border-b-2 w-[10rem] selection:bg-gray-600"/>
                     <button onClick={() => setIsMenuOpen(s => !s)} className="cursor-pointer outline-none pr-2 pl-2">
                         <FontAwesomeIcon className="text-2xl place-self-center mr-1" icon={faEllipsisVertical} />
                     </button>
@@ -29,9 +28,9 @@ function NoteCard({title, text, path, id}: {title: string, text: string, path: s
                 {
                     isMenuOpen ?
                         <div className="h-[8em] text-xl flex p-1 flex-col text-inactive">
-                            <button onClick={() => deleteNote(id)} className="cursor-pointer outline-none hover:text-active w-fit p-1">Delete</button>
-                            <button className="cursor-pointer outline-none hover:text-active w-fit p-1">Foo</button>
-                            <button className="cursor-pointer outline-none hover:text-active w-fit p-1">Bar</button>
+                            <button onClick={() => deleteNote(id)} className="cursor-pointer outline-none hover:text-active w-fit p-1 place-self-end">Delete</button>
+                            <button className="cursor-pointer outline-none hover:text-active w-fit p-1 place-self-end">Foo</button>
+                            <button className="cursor-pointer outline-none hover:text-active w-fit p-1 place-self-end">Bar</button>
                         </div> :
                         <Link to={path}>
                             <p className="h-[10em] p-2 overflow-clip shadow-fade shadow-element">{text}</p>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,29 @@
 import NoteCard from "../elements/NoteCard.tsx";
 import NoteCreationButton from "../elements/NoteCreationButton.tsx";
 import {useStore} from "../utils/AppState.ts";
-import {useEffect} from "react";
+import {useEffect, useRef, useState} from "react";
 
 
 function HomePage() {
 
     const notes = useStore((state) => state.notes);
+    const prevLength = useRef(notes.length);
+    //not to focus on input element when reloading the page (prolly can be done much easier, but idc)
+    const [grace, setGrace] = useState(true);
+    const elementToFocusOn = useRef<HTMLFormElement>(null);
     const getSavedNotes = useStore((s) => s.getNotesFromDB);
 
     useEffect(() => {
-        getSavedNotes();
+        getSavedNotes().then(() => setGrace(false));
     }, []);
+
+    useEffect(() => {
+        if (notes.length > prevLength.current && !grace) {
+            elementToFocusOn.current!.focus();
+            elementToFocusOn.current!.select();
+        }
+        prevLength.current = notes.length;
+    }, [notes]);
 
     return (
         <>
@@ -20,7 +32,7 @@ function HomePage() {
             </header>
             <main className="grid grid-cols-5 p-5 max-md:flex flex-col">
                 {notes.map((note) => <NoteCard key={note.id} title={note.title} text={note.content}
-                                                      path={`/note/${note.id}`} id={note.id}/>)}
+                                                      path={`/note/${note.id}`} id={note.id} ref={elementToFocusOn}/>)}
                 <NoteCreationButton/>
             </main>
         </>
